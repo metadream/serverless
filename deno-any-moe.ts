0da37051950b04a10ab6c70f9881a734755511d3
openflake <app@zerg.cc>
2023-02-01 22:01:12 +0800

    add

---
+++ deno-any-moe.ts
@@ -0,0 +1,21 @@
+import { serve } from "https://deno.land/std@0.139.0/http/server.ts";
+
+const html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
+  <title>Hello, Anybody!</title>
+  <style>body{margin:0;height:100vh;display:flex;align-items:center;justify-content:center;}svg{width:120px;}</style>
+</head>
+<body>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 39"><path d="M0 26 15 .001h15l7.5 13L45 0h15l7.5 13L75.006.01 90 0l7.489 13.018L105 0h15L97.5 39h-15l7.483-12.973-7.494-13.009L75 26H60l-7.5-13-7.517 13.027L30 26.001l-7.511-12.982L15 26.001Z" fill="#3c1e3c"/></svg>
+</body>
+</html>`
+  .replace(/(\r|\n)[\t ]+/g, "")
+  .replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g, "");
+
+serve((): Response => {
+  return new Response(html, { headers: { "content-type": "text/html" } });
+});
---
+++ deno-jsdelivr-proxy.ts
@@ -0,0 +1,23 @@
+import { serve } from "https://deno.land/std@0.139.0/http/server.ts";
+const upstream = "https://cdn.jsdelivr.net/gh/seatwork";
+
+serve(async (req: Request): Promise<Response> => {
+  let path = (new URL(req.url)).pathname;
+  if (path.startsWith("/gh/seatwork/")) {
+    path = path.replace("/gh/seatwork/", "/");
+  }
+  if (path.startsWith("/lib")) {
+    path = path.replace("/lib", "/front-end-libs");
+  }
+
+  switch (path) {
+    case "/":
+      return new Response("Upstream: https://cdn.jsdelivr.net");
+    case "/favicon.ico":
+      return new Response('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300"><path d="M1 58v72c0 60 0 73 2 80 7 42 38 75 80 86 25 5 54 2 76-8l101-59c21-16 35-41 38-66l1-80V12L279 1l-79 45-1 78c0 82 0 83-5 96-3 9-3 9-12 7-25-7-47-23-57-49-6-15-5-21-5-93V12L100 1zm99 36c0 55-1 65 6 91 11 27 37 51 66 60l6 3c0 1-11 12-20 17a84 84 0 01-52 14c-33-1-62-21-76-50-9-19-8-13-9-91V69l78-45zm179-1c-1 78 0 75-9 92a79 79 0 01-57 43l3-10c6-26 3-57 3-85l1-75 58-34z" fill="#00c250"/></svg>', {
+        headers: { "content-type": "image/svg+xml", "cache-control": "public, max-age=31536000" }
+      });
+    default:
+      return await fetch(new Request(upstream + path), req);
+  }
+});
