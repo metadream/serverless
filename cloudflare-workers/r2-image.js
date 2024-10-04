0e5c885702854d265a542edbb5eaaff62cd400e2
metadream <45996264+metadream@users.noreply.github.com>
2024-10-04 16:24:48 +0800

    Update r2-image.js

--- cloudflare-workers/r2-image.js
+++ cloudflare-workers/r2-image.js
@@ -3,7 +3,7 @@
  * Transform Images in R2 with Workers
  */
 export default {
-    r2_hostname: "pub-xxxxxxxxxxxxxxxxxxxxxxxxxxx.r2.dev",
+    r2_hostname: "pub-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.r2.dev",
 
     async fetch(request) {
         const url = new URL(request.url)
@@ -13,21 +13,23 @@ export default {
             return new Response("(c) 2024 Object storage service based on Cloudflare R2")
         }
         // No image or no querystring returns the raw content
-        if (!/\.(jpe?g|png|gif|webp)$/i.test(url.pathname) || !url.searchParams.toString()) {
+        const params = url.searchParams;
+        if (!/\.(jpe?g|png|gif|webp)$/i.test(url.pathname) || !params.toString()) {
             return fetch(new Request(url.href, { headers: request.headers }))
         }
 
         // Transform images options
-        const options = { cf: { image: { fit: "scale-down" } } }
-        if (url.searchParams.has("size")) {
-            options.cf.image.width = options.cf.image.height = url.searchParams.get("size")
+        const image = { fit: "scale-down" }
+        if (params.has("size")) {
+            image.width = image.height = params.get("size")
         }
-        if (url.searchParams.has("width")) options.cf.image.width = url.searchParams.get("width")
-        if (url.searchParams.has("height")) options.cf.image.height = url.searchParams.get("height")
-        if (url.searchParams.has("quality")) options.cf.image.quality = url.searchParams.get("quality")
-        if (url.searchParams.has("fit")) options.cf.image.fit = url.searchParams.get("fit")
-        
+        if (params.has("width")) image.width = params.get("width")
+        if (params.has("height")) image.height = params.get("height")
+        if (params.has("quality")) image.quality = params.get("quality")
+        if (params.has("fit")) image.fit = params.get("fit")
+
         const imageUrl = url.origin + url.pathname
+        const options = { cf: { image } }
         return fetch(new Request(imageUrl, { headers: request.headers }), options)
     }
 
