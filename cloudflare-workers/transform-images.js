436434c33f23c42e64d28e266182877881db01e8
metadream <45996264+metadream@users.noreply.github.com>
2024-10-04 16:15:45 +0800

    Update transform-images.js

--- cloudflare-workers/transform-images.js
+++ cloudflare-workers/transform-images.js
@@ -1,10 +1,23 @@
+/**
+ * Cloudflare Service Integration
+ * Transform Images in R2 with Workers
+ */
 export default {
+    r2_hostname: "pub-c5cb3e03a15d442885a8cca7767278d5.r2.dev",
+
     async fetch(request) {
         const url = new URL(request.url)
-        if (!/\.(jpe?g|png|gif|webp)$/i.test(url.pathname)) {
-            return new Response("Disallowed file extension", { status: 400 })
+        url.hostname = this.r2_hostname
+
+        if (url.pathname === "/") {
+            return new Response("(c) 2024 Object storage service based on Cloudflare R2")
+        }
+        // No image or no querystring returns the raw content
+        if (!/\.(jpe?g|png|gif|webp)$/i.test(url.pathname) || !url.searchParams.toString()) {
+            return fetch(new Request(url.href, { headers: request.headers }))
         }
 
+        // Transform images options
         const options = { cf: { image: { fit: "scale-down" } } }
         if (url.searchParams.has("size")) {
             options.cf.image.width = options.cf.image.height = url.searchParams.get("size")
@@ -13,12 +26,9 @@ export default {
         if (url.searchParams.has("height")) options.cf.image.height = url.searchParams.get("height")
         if (url.searchParams.has("quality")) options.cf.image.quality = url.searchParams.get("quality")
         if (url.searchParams.has("fit")) options.cf.image.fit = url.searchParams.get("fit")
-
-        url.hostname = "original.example.com"
+        
         const imageUrl = url.origin + url.pathname
-        const imageRequest = new Request(imageUrl, {
-            headers: request.headers
-        })
-        return fetch(imageRequest, options)
+        return fetch(new Request(imageUrl, { headers: request.headers }), options)
     }
+
 }
